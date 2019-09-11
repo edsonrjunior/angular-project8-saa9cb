06e0deac878a82f39b726bed53e146c3eafc5a1c
edsonrjunior <edsonkjr@gmail.com>
2019-09-10 21:45:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-project8-saa9cb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-project8-saa9cb)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class CartService {
+  items = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+  removeItem(product){
+    var index = this.items.indexOf(product);
+    this.items.splice(index,1);
+  }
+
+}
